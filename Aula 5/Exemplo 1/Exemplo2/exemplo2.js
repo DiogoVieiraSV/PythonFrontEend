a4acd9436e545b74c0502e9411b3a6689f2ffebe
Diogo Vieira <diogovieira708@gmail.com>
2024-11-04 21:52:24 -0300

    Aula 5 Frontend

---
+++ Aula 5/Exemplo 1/Exemplo2/exemplo2.js
@@ -0,0 +1,13 @@
+document.write('ola terra')
+//alert('ola');
+console.log('ola, tudo bem?');
+var variavel1 = 'olá mundo';
+//alert(variavel1);
+var variavel2 = 123;
+//alert(variavel2);
+console.log(variavel2);
+let variavel13 = 'Eu tambem sou uma variavel';
+//alert(variavel3);
+document.write("alo morte")
+document.write('<h1>Eu sou um Título</h1>')
+document.write('<title> titulo da pagina </title>')
